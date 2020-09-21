b487f9509493821096f0a780d77ed1d8e3c43a4e
Mike Peel <email@mikepeel.net>
2020-09-21 17:19:04 +0100

    +

--- smoothmaps/run_smoothnoisemap2018_pol_QU.py
+++ smoothmaps/run_smoothnoisemap2018_pol_QU.py
@@ -27,8 +27,8 @@ mapnumbers = [4, 7, 9, 8] # II,QQ,UU,QU
 rescale = 1000.0 # Convert from K to mK - this is applied to the noise map
 units_out = 'mK' # Must match rescale!
 numres = len(output_resolution)
-directory = '/Users/mpeel/Documents/maps/'
-# directory = '/scratch1/mpeel/maps/'
+# directory = '/Users/mpeel/Documents/maps/'
+directory = '/scratch1/mpeel/maps/'
 outdirectory = directory+"planck2018_tqu_noise_v0.6/"
 os.makedirs(outdirectory, exist_ok=True)
 

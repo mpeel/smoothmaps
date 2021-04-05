eca0720cb9a2bcf663b71506c0e2ff9b27672de2
Mike Peel <email@mikepeel.net>
2021-04-05 14:04:04 +0100

    +

--- smoothmaps/run_smoothnoisemap_wmap_pol_QU.py
+++ smoothmaps/run_smoothnoisemap_wmap_pol_QU.py
@@ -18,7 +18,7 @@ units_out = 'mK' # MUST MATCH sigma_0 AND sigma_P UNITS BELOW
 # directory = '/Users/mpeel/Documents/maps/'
 # directory = '/scratch1/mpeel/maps/'
 directory = '/share/nas_cbassarc/mpeel/'
-outdirectory = directory+"wmap9_tqu_noise_v0.9/"#_10k/"
+outdirectory = directory+"wmap9_tqu_noise_v1.0/"#_10k/"
 os.makedirs(outdirectory, exist_ok=True)
 # Read in the beams
 beamtf_K = np.loadtxt(directory+'wmap9/wmap_ampl_bl_K1_9yr_v5p1.txt',usecols=(1,))

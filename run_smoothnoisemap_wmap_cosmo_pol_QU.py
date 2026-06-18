dbfa1d311a7354a572a9dc8ea8d8cc1979e7d800
Mike Peel <email@mikepeel.net>
2026-06-18 16:27:47 +0100

    +

--- run_smoothnoisemap_wmap_cosmo_pol_QU.py
+++ run_smoothnoisemap_wmap_cosmo_pol_QU.py
@@ -15,10 +15,9 @@ mapnumbers = [3,4, 5, 6] # II,QQ,UU,QU
 rescale = 1.0 # Input maps are mK already
 units_out = 'mK' # Must match rescale!
 numres = len(output_resolution)
-# directory = '/Users/mpeel/Documents/maps/'
 directory = '/mnt/DATA1/mpeel/smoothmaps/'
 indirectory = directory + "cosmoglobe_dr1/"
-outdirectory = directory+"planck2020_tqu_noise_v1.0_10k/"
+outdirectory = directory+"wmap_cosmoglobe_tqu_noise_v1.0_1k/"
 os.makedirs(outdirectory, exist_ok=True)
 
 # Read in the beams
@@ -36,4 +35,13 @@ beamtf_W4 = np.loadtxt(indirectory+'wmap_ampl_bl_W4_9yr_v5p1.txt',usecols=(1,))
 for i in range(0,numres):
 	resolution = "%.2f" % output_resolution[i]
 
-	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_PlanckR4fullbeam_28.4_1024_2020_mKCMBunits', 'CG_023-WMAP_K_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmo_22.8_512_2023_mKCMBunits', 'CG_023-WMAP_K_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmo_33.0_512_2023_mKCMBunits', 'CG_030-WMAP_Ka_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq1_40.7_512_2023_mKCMBunits', 'CG_040-WMAP_Q1_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq2_40.7_512_2023_mKCMBunits', 'CG_040-WMAP_Q2_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmov1_60.7_512_2023_mKCMBunits', 'CG_060-WMAP_V1_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmov2_60.7_512_2023_mKCMBunits', 'CG_060-WMAP_V2_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq1_93.5_512_2023_mKCMBunits', 'CG_090-WMAP_W1_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq2_93.5_512_2023_mKCMBunits', 'CG_090-WMAP_W2_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq3_93.5_512_2023_mKCMBunits', 'CG_090-WMAP_W3_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
+	smoothnoisemap(indirectory, outdirectory, resolution+'smoothed_wmap9cosmoq4_93.5_512_2023_mKCMBunits', 'CG_090-WMAP_W4_IQU_n0512_v1.fits',mapnumber=mapnumbers,numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside[output_nside<=1024],windowfunction=beamtf_K,rescale=rescale,do_intensity=True,do_polarisation=True,units_out=units_out,usehealpixfits=True)
